f88077ecec89b510805bdedffa8e4ee912b77604
Jyoth Bhoga <jyothbhoga@gmail.com>
2025-07-21 06:51:05 +0000

    Initial commit

---
+++ generateToken.ts
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const timestamp = Date.now() + 60 * 60 * 1000;
+
+const payload = {
+  sub: "01961f0d-2f68-759e-9d05-5f67f87e0bdd",
+  iat: timestamp,
+  exp: timestamp,
+  aud: "30d18abe-e51e-4664-918a-bee51ee664fd",
+};
+
+const secret =
+  "54c4577ad1fa36f3768995ba00bc33135c630ab6f698ae0ca5e38dbc53e91fa7";
+
+const token = jwt.sign(payload, secret, { algorithm: "HS256" });
+
+console.log("Your JWT token:");
+console.log(token);
